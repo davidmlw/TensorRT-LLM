dd4f4d0eddbe73f0fa13d2a2307b5e1def5ba372
Yuan Tong <13075180+tongyuantongyu@users.noreply.github.com>
2025-07-11 16:52:19 +0800

    feat: reset KV cache reuse state
    Signed-off-by: Yuan Tong <13075180+tongyuantongyu@users.noreply.github.com>

--- cpp/include/tensorrt_llm/batch_manager/kvCacheManager.h
+++ cpp/include/tensorrt_llm/batch_manager/kvCacheManager.h
@@ -735,6 +735,12 @@ public:
         return 0;
     }
 
+    void resetReuseState()
+    {
+        mContextBlocksByHash.clear();
+        mCachedBlocksRoot = std::make_shared<KVCacheBlock>(KVCacheBlock::kCachedBlocksRootId, tensorrt_llm::kernels::KVCacheIndex{0});
+    }
+
 private:
     //! \brief Add single block to beam of sequence and mAllocatedBlocksPerSeq.
     void addBlockToBeam(BlockPtr& block, GenerationRequest& sequence, SizeType32 beamIdx);
@@ -1120,6 +1126,13 @@ public:
         return mWindowBlockManagers.at(windowSize).getPool(relativePoolIndex);
     }
 
+    void resetReuseState()
+    {
+        for (auto& [windowSize, manager] : mWindowBlockManagers)
+        {
+            manager.resetReuseState();
+        }
+    }
 private:
     [[nodiscard]] WindowBlockManager const& windowManagerByLayer(SizeType32 layerIdx) const
     {
@@ -1290,6 +1303,7 @@ public:
 
     virtual void refreshBlocks() = 0;
     virtual void flushIterationEvents() = 0;
+    virtual void resetReuseState() = 0;
 
     [[nodiscard]] static SizeType32 getSinkBubbleLength(SizeType32 sinkTokenLen, SizeType32 tokensPerBlock);
 
@@ -1631,6 +1645,11 @@ public:
     void flushIterationEvents() override
     {
         mBlockManager.flushIterationEvents();
+    }
+
+    void resetReuseState() override
+    {
+        mBlockManager.resetReuseState();
     }
 
     /// @brief Finds the maximum attention window that can be used on a sequence, given some kv-cache block capacity.
