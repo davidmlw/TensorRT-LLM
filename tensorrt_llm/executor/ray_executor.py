0971b8c030557fde20d18e1eaab55d979c4b2eb4
Liwei Ma <liweim@nvidia.com>
2025-10-29 15:48:13 +0800

    ray + updatew works
    trtllm works in async env
    
    trtllm works in sync and async env
    
    ray + updatew works
    
    rebase to the updated verl
    
    server mode
    
    still cherry pick
    
    still cherry pick
    
    still cherry pick
    
    integrated http interface
    
    hang at RyExecutor create workers ray.remote
    
    clean code
    
    use tensorrt_llm.rlhf_utils

--- tensorrt_llm/executor/ray_executor.py
+++ tensorrt_llm/executor/ray_executor.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from typing import Any, Dict, List, Optional, Tuple
 
 try:
@@ -23,6 +24,7 @@ from .ray_gpu_worker import RayGPUWorker, RayWorkerWrapper
 from .request import GenerationRequest
 from .result import GenerationResult
 from .rpc_proxy_mixin import RpcExecutorMixin
+from .utils import has_event_loop
 
 __all__ = [
     "RayExecutor",
@@ -77,17 +79,18 @@ class RayExecutor(RpcExecutorMixin, GenerationExecutor):
             self.master_address = ray.util.get_node_ip_address()
             self.master_port = get_free_port()
 
-            worker_kwargs = dict(**worker_kwargs,
+            self.worker_kwargs = dict(**worker_kwargs,
                                  postproc_worker_config=postproc_worker_config,
                                  is_llm_executor=is_llm_executor)
 
             self.init_rpc_executor()
-            worker_kwargs['rpc_addr'] = self.rpc_addr
-            self.create_workers(RayGPUWorker, worker_kwargs)
-            self.setup_engine_remote()
-            self.setup_mainloop(tasks=[self._fetch_responses_loop_async],
-                                thread_name="ray_executor_main_loop")
-            logger.info(f"Connecting to RPC server at {self.rpc_addr}")
+            self.worker_kwargs['rpc_addr'] = self.rpc_addr
+            if not has_event_loop():
+                self.init_workers_sync()
+                self.setup_engine_remote()
+                self.setup_mainloop(tasks=[self._fetch_responses_loop_async],
+                                    thread_name="ray_executor_main_loop")
+                logger.info(f"Connecting to RPC server at {self.rpc_addr}")
 
         except Exception as e:
             self.shutdown()
@@ -122,6 +125,8 @@ class RayExecutor(RpcExecutorMixin, GenerationExecutor):
             for rank in range(self.world_size)
         ]
 
+    def init_workers_sync(self):
+        self.create_workers(RayGPUWorker, self.worker_kwargs)
         try:
             ray.get([worker.__ray_ready__.remote() for worker in self.workers])
         except ray.exceptions.ActorDiedError as e:
@@ -130,6 +135,23 @@ class RayExecutor(RpcExecutorMixin, GenerationExecutor):
                 raise RuntimeError(
                     "RayGPUWorker died during initialization") from e
             raise
+
+    async def init_workers_async(self):
+        self.create_workers(RayGPUWorker, self.worker_kwargs)
+        try:
+            await asyncio.gather(*[worker.__ray_ready__.remote() for worker in self.workers])
+        except ray.exceptions.ActorDiedError as e:
+            if "The actor died because of an error raised in its creation task" in str(
+                    e):
+                raise RuntimeError(
+                    "RayGPUWorker died during initialization") from e
+            raise
+
+        self.setup_engine_remote()
+        self.setup_mainloop(tasks=[self._fetch_responses_loop_async],
+                            thread_name="ray_executor_main_loop")
+        logger.info(f"Connecting to RPC server at {self.rpc_addr}")
+
 
     @unwrap_ray_errors()
     def call_all_ray_workers(self, func: str, leader_only: bool,
